89e719b35bc3f2d8b08040d2c36cf15f74cec762
sultan99 <sultan99@mail.ru>
2019-06-17 13:17:13 +0600

    Function createReducer added.

--- src/common/utils.js
+++ src/common/utils.js
@@ -1,3 +1,20 @@
+import * as R from 'ramda'
+
+const toReducer = ([actionType, reducer]) => ([action, state]) => {
+  if (actionType === action.type) {
+    const result = reducer(action, state)
+    const newState = R.is(Function, result) ? result(state) : result
+    return [action, newState]
+  }
+  return [action, state]
+}
+
+export const createReducer = (initialState, ...reducers) => R.pipe(
+  (state, action) => [action, state || initialState],
+  ...reducers.map(toReducer),
+  R.last
+)
+
 export function makeCacheable(fn) {
   const cache = {}
   return async key => {
--- src/components/icon/svg/index.js
+++ src/components/icon/svg/index.js
@@ -3,6 +3,7 @@ const req = require.context(`.`, true, /\.svg$/)
 const iconList = req.keys().reduce((list, key) => {
   const icon = key.replace(/\.\/(.+).svg/, `$1`)
   list[icon] = req(key).default
+
   return list
 }, {})
 
