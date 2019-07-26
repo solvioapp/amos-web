d9b45583479657207269e0f90cfe2c94c2912d0f
dteiml <dteiml@gmail.com>
2019-07-26 15:32:32 +0200

    change footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -3,7 +3,7 @@ import Footer from './footer.sc'
 
 const AppFooter = () => (
   <Footer>
-    Built with ❤️ and ☕️ by <a href="https://solvio.org">Solvio Foundation</a>.
+    Built with ❤️ and ☕️ by <a target="_blank" href="http://solvio.org">Solvio Foundation</a>. <a target="_blank" href="https://github.com/solviofoundation/amos/wiki">Help.</a>
   </Footer>
 )
 
