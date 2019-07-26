0a3c9600e372d5c1e77099c5fcb8fa529a1a3cce
dteiml <dteiml@gmail.com>
2019-07-26 12:55:19 +0200

    add amos.gif

--- src/components/amos-chat/chat-box/index.js
+++ src/components/amos-chat/chat-box/index.js
@@ -2,7 +2,8 @@ import Avatar from './avatar.sc'
 import Box from './box.sc'
 import Bubble from '../bubble'
 import React from 'react'
-import image from './amos.png'
+// import image from './amos.png'
+import image from './amos.gif'
 
 const ChatBox = ({text}) => (
   <Box>
