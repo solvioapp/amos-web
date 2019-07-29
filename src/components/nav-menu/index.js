7eb358e4b92f8c1f46dc9a8973ca79d48ca8e9c4
dteiml <dteiml@gmail.com>
2019-07-29 16:52:23 +0200

    add log-in nav element

--- src/components/nav-menu/index.js
+++ src/components/nav-menu/index.js
@@ -1,18 +1,25 @@
 import Link from './link.sc'
 import Menu from './menu.sc'
 import React from 'react'
+import {withRouter} from 'react-router-dom'
 
-const NavMenu = ({isAuthenticated}) => (
-  <Menu>
-    <Link to="/review">Review</Link>
-    <Link to="/search">Search</Link>
-    <Link to="/notifications" hidden={!isAuthenticated}>
-      Notifications
-    </Link>
-    <Link to="/sign-up" hidden={isAuthenticated}>
-      Sign up
-    </Link>
-  </Menu>
-)
+const NavMenu = ({isAuthenticated, history: {location: {pathname: path}}}) => {
+  const logIn = path === '/log-in'
+  return (
+    <Menu>
+      <Link to="/review">Review</Link>
+      <Link to="/search">Search</Link>
+      <Link to="/notifications" hidden={!isAuthenticated}>
+        Notifications
+      </Link>
+      <Link to="/sign-up" hidden={isAuthenticated || logIn}>
+        Sign up
+      </Link>
+      <Link to='/log-in' hidden={isAuthenticated || !logIn}>
+        Log in
+      </Link>
+    </Menu>
+  )
+}
 
-export default NavMenu
+export default withRouter(NavMenu)
