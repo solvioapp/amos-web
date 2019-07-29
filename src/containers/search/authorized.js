9ec73d8eeb40444ba8bd63f9d12aec407b1051eb
dteiml <dteiml@gmail.com>
2019-07-29 15:16:14 +0200

    remove Introduce me to your friends

--- src/containers/search/authorized.js
+++ src/containers/search/authorized.js
@@ -5,7 +5,7 @@ const messages = [
   `Hi again.`,
   `Psst Psst 🤫 The guys from Solvio said they'll teach me search when we have 10,000 "data points". And if we can't get there they'll get rid of me! 😱 They call this "iteration".. 😳`,
   `So I'm in desperate need of your help! Go ahead and submit reviews for your favorite learning resources.`,
-  `Oh and introduce me to your friends. My address is www.amos.ms... Let's keep this product alive 😉`,
+  // `Oh and introduce me to your friends. My address is www.amos.ms... Let's keep this product alive 😉`,
 ]
 
 const Authorized = () => (
