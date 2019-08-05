d834e21e9ee128d5ca2f88c7d038b0940174ef52
sultan99 <sultan99@mail.ru>
2019-08-05 18:45:33 +0600

    Updated text of profile page.

--- src/containers/profile/index.js
+++ src/containers/profile/index.js
@@ -1,16 +1,23 @@
+import AmosChat from 'components/amos-chat'
 import Button from 'components/button'
-import Panel from './panel.sc'
 import React from 'react'
-import Title from 'components/title'
+import Top_ from './top.sc'
 import connect from './connect'
 
-const Notifications = ({logout}) => (
-  <Panel>
-    <Title>Notifications</Title>
-    <Button onClick={logout}>
-      Logout
-    </Button>
-  </Panel>
+const messages = [
+  `Unfortunately I can't show your profile just yet.`,
+  `Don't leave there's work to do 🧐`
+]
+
+const Profile = ({logout}) => (
+  <Top_>
+    <AmosChat callToAction={
+      <Button primary onClick={logout}>
+        Logout
+      </Button>}>
+      {messages}
+    </AmosChat>
+  </Top_>
 )
 
-export default connect(Notifications)
+export default connect(Profile)
