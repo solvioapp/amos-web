67d4958800d7a1d2eccf873649ffe96bbf378aa4
Velenir <velenir@gmail.com>
2019-09-03 14:52:40 +0300

    fix to prop for Redirect

--- src/containers/app/private-route.js
+++ src/containers/app/private-route.js
@@ -11,7 +11,7 @@ const PrivateRoute = ({isAuthenticated, component: Component, ...rest}) => (
   <Route {...rest} render={
     props => isAuthenticated
       ? <Component {...props}/>
-      : <Redirect to={destUrl}/>
+      : <Redirect to={destUrl(props)}/>
   }/>
 )
 
