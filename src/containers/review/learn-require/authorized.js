6cd8b8d712cb25d24d7f033b6f7e6b855acdfb4d
Velenir <velenir@gmail.com>
2019-09-05 16:01:32 +0300

    use review state in /review/requirements

--- src/containers/review/learn-require/authorized.js
+++ src/containers/review/learn-require/authorized.js
@@ -1,34 +1,35 @@
 import AmosChat from 'components/amos-chat'
 import Title from 'components/title'
 import Button from 'components/button'
-import Input from 'components/input'
 import Buttons from '../buttons.sc'
 import Top_ from '../top.sc'
 import InputForm_ from '../input-form.sc'
-import {navto} from 'common/history'
 import React from 'react'
+import {useReqState} from './'
 
 import ReqList from './ReqList'
 
-const Authorized = ({...rest}) => (
-  <Top_ {...rest}>
+const Authorized = ({...rest}) => {
+  const {initItems, goFinish, goPrev, setRequirements} = useReqState()
+
+  return <Top_ {...rest}>
     <AmosChat>
       What do you need to know in order to make the most out of this resource?
       Again, try to be as detailed as possible.
     </AmosChat>
     <InputForm_>
       <Title>Learning requirements</Title>
-      <ReqList/>
+      <ReqList onUpdate={setRequirements} initItems={initItems}/>
       <Buttons>
-        <Button onClick={navto(`/review/topics`)}>
+        <Button onClick={goPrev}>
           Previous
         </Button>
-        <Button primary onClick={navto(`/review/thanks`)}>
+        <Button primary onClick={goFinish}>
           Finish
         </Button>
       </Buttons>
     </InputForm_>
   </Top_>
-)
+}
 
 export default Authorized
--- src/containers/review/learn-require/guest.js
+++ src/containers/review/learn-require/guest.js
@@ -4,30 +4,34 @@ import Button from 'components/button'
 import Buttons_ from '../buttons.sc'
 import Top_ from '../top.sc'
 import InputForm_ from '../input-form.sc'
-import {navto} from 'common/history'
 import React from 'react'
+import {useReqState} from './'
 
 import ReqList from './ReqList'
 
-const Guest = ({...rest}) => (
-  <Top_ {...rest}>
-    <AmosChat>
+const Guest = ({...rest}) => {
+  const {initItems, goFinish, goPrev, setRequirements} = useReqState()
+
+  return (
+    <Top_ {...rest}>
+      <AmosChat>
       What do you need to know in order to make the most out of this resource?
       Again, try to be as detailed as possible.
-    </AmosChat>
-    <InputForm_>
-      <Title>Learning requirements</Title>
-      <ReqList/>
-      <Buttons_>
-        <Button onClick={navto(`/review/topics`)}>
+      </AmosChat>
+      <InputForm_>
+        <Title>Learning requirements</Title>
+        <ReqList onUpdate={setRequirements} initItems={initItems}/>
+        <Buttons_>
+          <Button onClick={goPrev}>
           Previous
-        </Button>
-        <Button primary onClick={navto(`/review/thanks`)}>
+          </Button>
+          <Button primary onClick={goFinish}>
           Finish
-        </Button>
-      </Buttons_>
-    </InputForm_>
-  </Top_>
-)
+          </Button>
+        </Buttons_>
+      </InputForm_>
+    </Top_>
+  )
+}
 
 export default Guest
